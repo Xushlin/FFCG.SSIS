d38289e80ff6d2ee06bf2352446e95d2d3d2171e
Erik Cedheim <cedheim@gmail.com>
2016-08-11 15:11:07 +0200

    Contract for starting packages.

---
+++ FFCG.SSIS.PackageRunner.Contract/Model/ObjectType.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjectType.cs" company="Erik Cedheim">
+//   Copyright 2016 Erik Cedheim
+// </copyright>
+// <summary>
+//   The object type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace FFCG.SSIS.PackageRunner.Contract.Model
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The object type.
+    /// </summary>
+    [DataContract(Name = "ObjectType")]
+    public enum ObjectType
+    {
+        /// <summary>
+        /// The server level.
+        /// </summary>
+        [EnumMember(Value = "Server")]
+        Server = 50,
+
+        /// <summary>
+        /// The project.
+        /// </summary>
+        [EnumMember(Value = "Project")]
+        Project = 20,
+
+        /// <summary>
+        /// The package.
+        /// </summary>
+        [EnumMember(Value = "Package")]
+        Package = 30
+    }
+}
