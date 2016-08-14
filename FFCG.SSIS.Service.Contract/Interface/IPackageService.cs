1493fc834fe8a2b97b64f09794be1046ea462056
Erik Cedheim <cedheim@gmail.com>
2016-08-14 13:23:14 +0200

    Service contract.

---
+++ FFCG.SSIS.Service.Contract/Interface/IPackageService.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IPackageService.cs" company="Erik Cedheim">
+//   Copyright 2016 Erik Cedheim
+// </copyright>
+// <summary>
+//   Defines the IPackageService type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace FFCG.SSIS.Service.Contract.Interface
+{
+    /// <summary>
+    /// The PackageService interface.
+    /// </summary>
+    public interface IPackageService
+    {
+        
+    }
+}
